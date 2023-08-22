64ea71ce3f2585f1bcc9450f81b5e9ef59cbc768
Bhavy Munjani <bhavymunjani1418@gmail.com>
2023-08-22 17:07:57 +0530

    Add files via upload
    first commit

---
+++ Array/twoSum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    //sort(ans.begin(),ans.end());
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
